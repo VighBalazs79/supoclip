1dd9f14e55870e282a1ad9a4f3c9da8ceb3960c0
kevaisolutions <kevai.solutions@gmail.com>
2026-05-30 15:04:39 +0200

    fix: install uv first

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.cargo/bin:$PATH"
 cd backend
-pip install uv
 uv sync
 uv run uvicorn src.main_refactored:app --host 0.0.0.0 --port ${PORT:-8000}
